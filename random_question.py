0ebf7cffffbf4da882853135b68377989c645dc5
Plasticomega <105556394+Plasticomega@users.noreply.github.com>
2022-05-14 21:15:58 +0530

    Add files via upload

---
+++ random_question.py
@@ -0,0 +1,68 @@
+import random
+
+questions = ['What is the smallest thing for which you are grateful?',
+             'Who has had the most positive impact on your life?',
+             'If you could use a time machine, would you rather have one that only goes back in time or only goes forward?',
+             'If you got a promotion, a job, a college acceptance, an accolade/award, or just generally accomplished something major, who is the first person youd tell and how do you think theyd react?',
+             'If you were an inanimate object, what would you be and why?',
+             'Where do you wish you had grown u',
+             'If you could be good at any profession without having to receive the accompanying education or trade experience, which would you choose?',
+             'What is something youre terrible at but wish you could do well?',
+             'What is the quality you admire the most in the person you dislike the most?',
+             'What was the most recent thing that made you cry',
+             'What are the books/movies/games that never get old and always make you feel better when you get down',
+             'What is the most trivial thing about which you have a strong opinion? ',
+             'If you could be any mythical creature, what would you be and why? ',
+             'If youve had more than one job, which job taught you the most?',
+             'If you could change one thing about yourself physically, what would you change? ',
+             'What single event or decision do you think most affected the rest of your life? Was there a turning point in your life?',
+             'What is the one thing youve made that youre the most proud of?',
+             'Some historical figures have epithets attached to their names, like The Mad or The Wise. What would you like yours to be?',
+             'What was the best year of your life? The worst? ',
+             'What one thing, whether its something you did or something you made or something you caused to happen, would you like to be remembered by?',
+             'What has been the biggest change of heart youve had? Have you ever started off on one side of an issue and wound up on the other? What influenced that change?',
+             'What is your biggest non-academic, non work-related accomplishment?',
+             'What is your biggest academic or work-related accomplishment? ',
+             'What is something youve done/felt/seen/etc. that you wish you could experience again for the first time?',
+             'If you could erase one thing or event from your memory, what would you choose?',
+             'Is there something about you that people assume because of your appearance or demeanour? What is a trait or preference you have that people dont expect you to have?',
+             'If you were a character in a movie, book, or television show, what genre would you live in?',
+             'What is the hardest way youve learned an important lesson?',
+             'What cliche do you think is bullshit? What cliche do you think holds truth?',
+             'What do you fear, despite having no real reason to do so? Basically, what is an irrational fear you have? ',
+             'Imagine that you could choose when and how you die. At what age would you like to die (no maximum here) and how would you like to go?',
+             'What character archetype is closest to your personality?',
+             'What was the happiest moment of your life so far?',
+             'What is one childhood memory that you remember especially well?',
+             'Is there a song/movie/food/etc. that strongly reminds you of someone whenever you experience it? Who does it remind you of?',
+             'What is your ideal birthday? Not the date, but rather your ideal way to spend the day. ',
+             'Are there any smells that bring back memories for you? What are they, and what memories do they elicit? ',
+             'What is the closest youve ever come to dying?',
+             'Do you have a personal mantra? If so, what is it?',
+             'What is the most memorable meal youve ever had? (It can be good or bad.) What made it so memorable?',
+             'Whats the worst advice youve ever been given',
+             'In what ways do you benefit the world?',
+             'What fact about yourself took you the longest to understand or accept?',
+             'What is your least popular or most controversial opinion?',
+             'If you could live in another time period but stay in the same place you live now, when would you want to live? ',
+             'Do you have any conspiracy theories? If so, what are they?'
+             'What is the strangest thing you believe in? Do you believe in the supernatural? Heaven, angels, ghosts? Luck, fate, magic? Mind reading, lizard people, the Illuminati? Where do you draw the line of belief and disbelief?',
+             'What fictional character do you most relate to?'
+
+
+             
+           
+
+
+           
+            
+            
+             ]
+
+
+value = random.choice(questions)
+print(value)
+
+
+
+
